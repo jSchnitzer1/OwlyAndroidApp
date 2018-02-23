e6a7764af5a1f6c4fd8d364413817b5fb1d607f4
khaled <khaledj@kth.se>
2018-02-23 19:52:17 +0100

    Facebook login + making the menu toolbar in home activity (with logout)

--- app/src/main/java/com/owly/owlyandroidapp/HomeActivity.java
+++ app/src/main/java/com/owly/owlyandroidapp/HomeActivity.java
@@ -1,14 +1,63 @@
 package com.owly.owlyandroidapp;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
-public class HomeActivity extends Activity {
+import com.facebook.AccessToken;
+import com.facebook.login.LoginManager;
+
+public class HomeActivity extends AppCompatActivity {
+
+    Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.home_menu, menu);
+        return true;
+    }
+
+    private boolean isLoggedInFacebook() {
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        return accessToken != null;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logout_item:
+                Intent i = new Intent(HomeActivity.this, LoginActivity.class);
+
+                if(isLoggedInFacebook()) {
+                    LoginManager.getInstance().logOut();
+                    i.putExtra("txtStatusMsg", "You are logged out Facebook successfully!");
+                }
+                startActivity(i);
+                break;
+            case R.id.profile_item:
+                break;
+            case R.id.about_us_item:
+                Toast.makeText(HomeActivity.this, "About us...", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/owly/owlyandroidapp/LoginActivity.java
+++ app/src/main/java/com/owly/owlyandroidapp/LoginActivity.java
@@ -5,29 +5,97 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 
 public class LoginActivity extends Activity {
+
+    TextView txtStatus;
+    LoginButton login_button;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initializeControls();
+        loginFB();
 
-        ImageView owlyIcon = (ImageView)findViewById(R.id.owlyIcon);
+        if(isLoggedIn())
+            goToHomeActivity();
+
+        ImageView owlyIcon = (ImageView) findViewById(R.id.owlyIcon);
         owlyIcon.setBackgroundResource(R.drawable.login_owly_icon);
 
-        ImageView owlyTxtImg = (ImageView)findViewById(R.id.owlyTxtImg);
+        ImageView owlyTxtImg = (ImageView) findViewById(R.id.owlyTxtImg);
         owlyTxtImg.setBackgroundResource(R.drawable.owly_text_main);
 
-        ImageView bankIdImg = (ImageView)findViewById(R.id.bankIdImg);
+        ImageView bankIdImg = (ImageView) findViewById(R.id.bankIdImg);
         bankIdImg.setBackgroundResource(R.drawable.bankid);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String txtStatusMsg = extras.getString("txtStatusMsg");
+            if(txtStatusMsg != null) {
+                txtStatus.setText(txtStatusMsg);
+            }
+        }
+
     }
 
-    public void login(View v){
-        if(v.getId() == R.id.login) {
-            Intent i = new Intent(LoginActivity.this, HomeActivity.class);
-            startActivity(i);
+    private void loginFB() {
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                txtStatus.setText("Successfully logged in Facebook");
+                goToHomeActivity();
+            }
+
+            @Override
+            public void onCancel() {
+                txtStatus.setText("Facebook Login canceled!");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                txtStatus.setText("Login error: " + exception.getMessage());
+            }
+        });
+    }
+
+    private void goToHomeActivity() {
+        Intent i = new Intent(LoginActivity.this, HomeActivity.class);
+        startActivity(i);
+    }
+
+    private void initializeControls() {
+        txtStatus = (TextView) findViewById(R.id.txtStatus);
+        login_button = (LoginButton) findViewById(R.id.login_button);
+        callbackManager = CallbackManager.Factory.create();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private boolean isLoggedIn() {
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        return accessToken != null;
+    }
+
+    public void login(View v) {
+        if (v.getId() == R.id.login) {
+            goToHomeActivity();
         }
     }
 }

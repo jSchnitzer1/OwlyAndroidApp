4ed65af3866343f8f8382118a46edd9dec67f56a
khaled <khaledj@kth.se>
2018-02-24 12:25:36 +0100

    Google login + fixes on Facebook login

--- app/src/main/java/com/owly/owlyandroidapp/HomeActivity.java
+++ app/src/main/java/com/owly/owlyandroidapp/HomeActivity.java
@@ -2,6 +2,8 @@ package com.owly.owlyandroidapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,20 +14,74 @@ import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
 import com.facebook.AccessToken;
+import com.facebook.Profile;
 import com.facebook.login.LoginManager;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
+    AccessToken accessToken;
 
-    Button logout;
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mAuth != null)
+            mAuth.addAuthStateListener(mAuthListener);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        initializeFacebook();
+        initializeGoogle();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        showLoginStatus();
+
+    }
+
+    private void showLoginStatus() {
+        if (isLoggedInFacebook()) {
+            Profile profile = Profile.getCurrentProfile();
+            if(profile != null) {
+                String firstName = profile.getFirstName();
+                Snackbar.make(findViewById(R.id.search), "Hi " + firstName + "\nYou are logged in using Facebook account", Snackbar.LENGTH_SHORT).show();
+            }
+            else {
+                Snackbar.make(findViewById(R.id.search), "Hi\nYou are logged in using Facebook account", Snackbar.LENGTH_SHORT).show();
+            }
+        } else if (isLoggedInGoogle()) {
+            GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+            if (acct != null) {
+                String personGivenName = acct.getGivenName();
+                Snackbar.make(findViewById(R.id.search), "Hi " + personGivenName + "\nYou are logged in using Google account", Snackbar.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+    private void initializeFacebook() {
+        accessToken = AccessToken.getCurrentAccessToken();
+    }
+
+    private void initializeGoogle() {
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() == null && !isLoggedInFacebook()) {
+                    goToLoginActivity("You are logged out Google successfully!");
+                }
+            }
+        };
     }
 
     @Override
@@ -36,21 +92,23 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private boolean isLoggedInFacebook() {
-        AccessToken accessToken = AccessToken.getCurrentAccessToken();
         return accessToken != null;
+    }
+
+    private boolean isLoggedInGoogle() {
+        return mAuth != null;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.logout_item:
-                Intent i = new Intent(HomeActivity.this, LoginActivity.class);
-
-                if(isLoggedInFacebook()) {
+                if (isLoggedInFacebook()) {
                     LoginManager.getInstance().logOut();
-                    i.putExtra("txtStatusMsg", "You are logged out Facebook successfully!");
+                    goToLoginActivity("You are logged out Facebook successfully!");
+                } else if (isLoggedInGoogle()) {
+                    mAuth.signOut();
                 }
-                startActivity(i);
                 break;
             case R.id.profile_item:
                 break;
@@ -59,5 +117,11 @@ public class HomeActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void goToLoginActivity(String msg) {
+        Intent i = new Intent(HomeActivity.this, LoginActivity.class);
+        i.putExtra("txtStatusMsg", msg);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/owly/owlyandroidapp/LoginActivity.java
+++ app/src/main/java/com/owly/owlyandroidapp/LoginActivity.java
@@ -3,9 +3,13 @@ package com.owly.owlyandroidapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
@@ -14,6 +18,20 @@ import com.facebook.FacebookException;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 
 public class LoginActivity extends Activity {
@@ -22,6 +40,20 @@ public class LoginActivity extends Activity {
     LoginButton login_button;
     CallbackManager callbackManager;
 
+    SignInButton gButton;
+    FirebaseAuth mAuth;
+    private final static int RC_SIGN_IN = 2;
+    GoogleSignInClient mGoogleSignInClient;
+    FirebaseAuth.AuthStateListener mAuthListener;
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,17 +61,18 @@ public class LoginActivity extends Activity {
         initializeControls();
         loginFB();
 
+        initializeGoogle();
+
         if(isLoggedIn())
             goToHomeActivity();
+
+        initializeGoogle();
 
         ImageView owlyIcon = (ImageView) findViewById(R.id.owlyIcon);
         owlyIcon.setBackgroundResource(R.drawable.login_owly_icon);
 
         ImageView owlyTxtImg = (ImageView) findViewById(R.id.owlyTxtImg);
         owlyTxtImg.setBackgroundResource(R.drawable.owly_text_main);
-
-        ImageView bankIdImg = (ImageView) findViewById(R.id.bankIdImg);
-        bankIdImg.setBackgroundResource(R.drawable.bankid);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
@@ -49,6 +82,82 @@ public class LoginActivity extends Activity {
             }
         }
 
+    }
+
+    private void initializeGoogle() {
+        gButton = (SignInButton) findViewById(R.id.g_signin);
+        mAuth = FirebaseAuth.getInstance();
+
+        gButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() != null) {
+                    goToHomeActivity();
+                }
+            }
+        };
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                Log.w("TAG", "Google sign in failed", e);
+                Toast.makeText(LoginActivity.this, "Authentication went wrong!", Toast.LENGTH_LONG).show();
+            }
+        }
+        else {
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithCredential:failure", task.getException());
+                            Snackbar.make(findViewById(R.id.main_layout), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
     }
 
     private void loginFB() {
@@ -82,12 +191,6 @@ public class LoginActivity extends Activity {
         callbackManager = CallbackManager.Factory.create();
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
     private boolean isLoggedIn() {
         AccessToken accessToken = AccessToken.getCurrentAccessToken();
         return accessToken != null;
@@ -98,4 +201,5 @@ public class LoginActivity extends Activity {
             goToHomeActivity();
         }
     }
+
 }

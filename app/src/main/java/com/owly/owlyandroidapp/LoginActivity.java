96df15af0b9b7e88601107e2591a5981f5f0f4a8
khaled <khaledj@kth.se>
2018-02-25 13:29:10 +0100

    Login fixes

--- app/src/main/java/com/owly/owlyandroidapp/LoginActivity.java
+++ app/src/main/java/com/owly/owlyandroidapp/LoginActivity.java
@@ -64,6 +64,8 @@ public class LoginActivity extends Activity {
         if(isLoggedIn())
             goToHomeActivity();
 
+        initializeGoogle();
+
         ImageView owlyIcon = (ImageView) findViewById(R.id.owlyIcon);
         owlyIcon.setBackgroundResource(R.drawable.login_owly_icon);
 
